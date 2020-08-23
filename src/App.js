09dfe720495aeebba397321527ffe6578ff4b5b5
Carlos Nuila <cnuila14@icloud.com>
2020-08-22 19:15:32 -0600

    Interfaz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./Color.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+            <div className="nav-wrapper fondoVerde1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col s8">
+                            <h5>Thoughts</h5>
+                        </div>
+                        <div className="col s4">
+                            <ul>
+                                <a className="btn waves-effect waves-light teal darken-3" >Agregar <i className="material-icons right">add</i></a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
   );
 }
---
+++ src/Components/Agregar.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+class Agregar extends React.Component{
+
+}
